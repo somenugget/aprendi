0c1a91f487a1fccf3ba307dd5d06896550559753
Dmitriy Shcherbakan <some.nugget@gmail.com>
2023-11-22 10:24:44 -0500

    Show the dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,27 @@
 class DashboardController < ApplicationController
   def index
+    terms_base = Term.where(study_set_id: current_user.study_sets)
+                     .joins(term_progresses_join_sql)
+                     .select('terms.*, term_progresses.tests_count, term_progresses.success_percentage, term_progresses.next_test_date')
+
+    @new_terms_to_learn = terms_base.where('term_progresses.success_percentage IS NULL').load
+
+    @ripe_terms_to_learn = terms_base.where("DATE_TRUNC('day', term_progresses.next_test_date) <= NOW() AND term_progresses.learnt = FALSE")
+                                     .where.not(id: @new_terms_to_learn.map(&:id))
+                                     .order('term_progresses.next_test_date NULLS FIRST, terms.created_at')
+                                     .limit(5)
+                                     .load
+
+    @ripening_terms = terms_base.where.not(id: @new_terms_to_learn.map(&:id))
+                                .where.not(id: @ripe_terms_to_learn.map(&:id))
+                                .order(created_at: :desc)
+  end
+
+  private
+
+  def term_progresses_join_sql
+    <<~SQL
+      LEFT JOIN term_progresses ON terms.id = term_progresses.term_id AND term_progresses.user_id = #{current_user.id}
+    SQL
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ApplicationRecord
   devise :omniauthable, omniauth_providers: %i[google_oauth2 facebook twitter]
 
   has_many :authorizations, dependent: :destroy
+  has_many :study_sets, dependent: :destroy
   has_many :folders, dependent: :destroy
   has_many :tests, dependent: :destroy
 
   class << self
+    # @param [OmniAuth::AuthHash] auth
     def from_omniauth(auth)
       authorization = Authorization.where(
         provider: auth.provider,
