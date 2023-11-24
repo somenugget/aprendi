e9227b1946e013742a1c78053ddb27be121ec73a
Dmitriy Shcherbakan <some.nugget@gmail.com>
2023-11-24 15:25:54 -0500

    Fix test

--- spec/components/ui/card_component_spec.rb
+++ spec/components/ui/card_component_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe Ui::CardComponent, type: :component do
+RSpec.describe UI::CardComponent, type: :component do
   pending "add some examples to (or delete) #{__FILE__}"
 
   # it "renders something useful" do
