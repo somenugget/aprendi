1a78f73063dd4001cf882e27fcc2629bdebb119d
Dmitriy Shcherbakan <some.nugget@gmail.com>
2023-11-26 08:43:35 -0500

    Revert "Test error controller"
    This reverts commit 7e902711b990030d424b39e95db7d1e36afde633.

--- app/controllers/error_controller.rb
+++
@@ -1,5 +0,0 @@
-class ErrorController < ApplicationController
-  def index
-    raise StandardError, 'This is a test error'
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'error/index'
   root 'home#index'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
