0cc57046f7d8e00b3b9fc9ff8d5439641b0390c4
Dmitriy Shcherbakan <some.nugget@gmail.com>
2023-12-04 12:50:36 -0500

    Rubocop

--- app/models/term_example.rb
+++ app/models/term_example.rb
@@ -1,7 +1,13 @@
 class TermExample < ApplicationRecord
   validates :term, :definition, :term_lang, :definition_lang, :term_example, :definition_example, presence: true
 
-  normalizes :term, :definition, :term_lang, :definition_lang, :term_example, :definition_example, with: -> { _1.downcase.strip.squish }
+  normalizes :term,
+             :definition,
+             :term_lang,
+             :definition_lang,
+             :term_example,
+             :definition_example,
+             with: -> { _1.downcase.squish }
 
   has_many :term_example_terms, dependent: :destroy
   has_many :terms, through: :term_example_terms
