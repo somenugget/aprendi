8948465db164dfc3ff8a044282a0b441650f25ae
Dmitriy Shcherbakan <some.nugget@gmail.com>
2023-12-08 07:58:58 -0500

    Concurrency control

--- app/jobs/generate_term_examples_job.rb
+++ app/jobs/generate_term_examples_job.rb
@@ -1,9 +1,16 @@
 class GenerateTermExamplesJob < ApplicationJob
+  include GoodJob::ActiveJobExtensions::Concurrency
+
   queue_as :generate_terms
 
   retry_on StandardError, wait: 5.minutes, attempts: 3
 
   discard_on ActiveRecord::RecordNotFound
+
+  good_job_control_concurrency_with(
+    perform_limit: 2,
+    key: -> { 'GenerateTermExamplesJob' }
+  )
 
   # @param [String] term_id
   def perform(term_id)
