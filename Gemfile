b70e7ae403147dfc4f45c2f66df9f1ae7c105e11
Dmitriy Shcherbakan <some.nugget@gmail.com>
2023-12-27 15:14:41 -0500

    Show wrong words diff in write word test

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ gem 'mailtrap'
 gem 'langchainrb'
 gem 'ruby-openai'
 
+gem 'diff-lcs'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
---
+++ app/components/test_steps/answer_diff_component.rb
@@ -0,0 +1,59 @@
+class TestSteps::AnswerDiffComponent < ApplicationComponent
+  attr_reader :correct, :wrong, :diff
+
+  class CorrectLetters < Array; end
+
+  class WrongLetters < Array; end
+
+  # @param [String] correct
+  # @param [String] wrong
+  def initialize(correct:, wrong:)
+    @correct = correct
+    @wrong = wrong
+    @diff = Diff::LCS.sdiff(correct, wrong)
+  end
+
+  # @return [String]
+  def call
+    content_tag :span do
+      if show_diff?
+        render_groups(letters_groups)
+      else
+        correct
+      end
+    end
+  end
+
+  private
+
+  def show_diff?
+    (diff || []).count { |d| d.action != '=' } <= correct.length / 2.0
+  end
+
+  def render_groups(groups)
+    groups.map do |group|
+      concat content_tag(:span, group.join, class: group.is_a?(WrongLetters) && 'font-extrabold')
+    end
+  end
+
+  def letters_groups
+    groups = []
+    current_group = CorrectLetters.new
+
+    diff.each do |d|
+      if d.action == '=' && current_group.is_a?(WrongLetters)
+        groups << current_group
+        current_group = CorrectLetters.new
+      end
+
+      if d.action != '=' && current_group.is_a?(CorrectLetters)
+        groups << current_group
+        current_group = WrongLetters.new
+      end
+
+      current_group.push d.old_element
+    end
+
+    groups << current_group
+  end
+end
