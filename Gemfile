98da30a6a34f503c9bee5810596c5e3b8e950b26
Dmitriy Shcherbakan <some.nugget@gmail.com>
2026-07-11 11:33:40 +0200

    Add sidekickicons via rails_icons, swap star for pin icon

--- Gemfile
+++ Gemfile
@@ -96,3 +96,5 @@ gem 'dockerfile-rails', '~> 1.5', :group => :development
 gem 'sentry-ruby', '~> 5.13'
 
 gem 'sentry-rails', '~> 5.13'
+
+gem 'rails_icons', '~> 1.9'
---
+++ config/initializers/rails_icons.rb
@@ -0,0 +1,21 @@
+RailsIcons.configure do |config|
+  config.default_library = 'sidekickicons'
+  # config.default_variant = "" # Set a default variant for all libraries
+
+  # Override Sidekickicons defaults
+  # config.libraries.sidekickicons.default_variant = "" # Set a default variant for Sidekickicons
+  # config.libraries.sidekickicons.exclude_variants = [:mini, :micro] # Exclude specific variants
+
+  # config.libraries.sidekickicons.outline.default.css = "size-6"
+  # config.libraries.sidekickicons.outline.default.stroke_width = "1.5"
+  # config.libraries.sidekickicons.outline.default.data = {}
+
+  # config.libraries.sidekickicons.solid.default.css = "size-6"
+  # config.libraries.sidekickicons.solid.default.data = {}
+
+  # config.libraries.sidekickicons.mini.default.css = "size-5"
+  # config.libraries.sidekickicons.mini.default.data = {}
+
+  # config.libraries.sidekickicons.micro.default.css = "size-4"
+  # config.libraries.sidekickicons.micro.default.data = {}
+end
